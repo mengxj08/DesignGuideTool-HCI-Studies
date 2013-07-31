019e1c4fe8a31e811667654de855885629f7ecc1
xgjonathan <xgjonathan@gmail.com>
2013-07-31 11:43:20 +0800

    data structure

---
+++ DesignToolWPF/DesignToolWPF/DataStructure.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignToolWPF
+{
+    class DataStructure
+    {
+        ResearchQuestion researchQuestion;
+        Arrangement arrangement;
+        List<IndependentVariable> independentVariables;
+        List<DependentVariable> dependentVariables;
+
+        public DataStructure()
+        {
+        }
+
+        // Construct from an existing xml file.
+        public DataStructure(String path)
+        {
+        }
+
+        // Store to an xml file.
+        public bool WriteToXml(String path)
+        {
+            return false;
+        }
+    }
+
+    class ResearchQuestion
+    {
+        string experimentTitle;
+        string experimentDescription;
+        string experimentConductor;
+
+        class Hypothesis
+        {
+            string mainSolution;
+            List<CompareSolution> compareSolutions;
+            List<Task> tasks;
+            string context;
+            List<Measure> measures;
+            string targetPopulation;
+
+
+            class CompareSolution
+            {
+                int id;
+                string name;
+            }
+
+            class Task
+            {
+                int id;
+                string name;
+            }
+
+            class Measure
+            {
+                int id;
+                string name;
+            }
+        }
+    }
+
+    class IndependentVariable
+    {
+        string name;
+        IVTYPE type;
+        COUNTERBALANCE counterBalance;
+        List<Level> levels;
+
+        public enum IVTYPE { Primary, Secondary };
+
+        public enum COUNTERBALANCE { None, LatinSquare, Full };
+        
+        class Level
+        {
+            int id;
+            string value;
+        }
+    }
+
+    class DependentVariable
+    {
+    }
+
+    class ControlVariable
+    {
+    }
+
+    class RandomVariable
+    {
+    }
+
+    class Arrangement
+    {
+    }
+}
